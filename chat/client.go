7292579653f5890c2bec5ef4ed7e305894eaeb88
malakaljamri <malakaljamri02@gmail.com>
2025-01-17 12:26:04 +0000

    fix sign_in

--- chat/client.go
+++ chat/client.go
@@ -15,8 +15,8 @@ import (
 
 const (
 	writeWait      = 10 * time.Second
-	pongWait       = 60 * time.Second
-	pingPeriod     = (pongWait * 9) / 10
+	pongWait       = 60 * time.Second 
+	pingPeriod     = (pongWait * 9) / 10 
 	maxMessageSize = 512
 )
 
